e4d1946364f904e76e1c83b6cd43e62d3ef41228
James Farrugia <jamesfarrugia@users.noreply.github.com>
2015-10-13 11:16:42 +0200

    Creating hello world with simple variable

---
+++ unit-2-variables-and-methods/HelloWorldVariable.java
@@ -0,0 +1,17 @@
+/**
+ * Hello world program.  Prints hello world to the screen and exits
+ */
+public class HelloWorld
+{
+	/**
+	 * Main method that prints the message
+	 * 
+	 * @param args array with command line arguments
+	 */
+	public static void main(String[] args)
+	{
+	  String myText = "Hello World!";
+		// Prints out "Hello World!"
+		System.out.println(myText);
+	}
+}
