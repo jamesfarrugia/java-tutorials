8e7fa7d5639ceb07b3ead8f4c4680f051942c963
James Farrugia <jamesfarrugia@users.noreply.github.com>
2015-10-13 11:54:56 +0200

    Creating hello world using an array of characters

---
+++ unit-2-variables-and-methods/HelloWorldCharArray.java
@@ -0,0 +1,18 @@
+/**
+ * Hello world program.  Prints hello world to the screen and exits
+ */
+public class HelloWorldCharArray
+{
+	/**
+	 * Main method that prints the message
+	 * 
+	 * @param args array with command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		// Declaring the text as an array of characters
+		char[] myTextArray = new char[]{'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+		// Prints out "Hello World!"
+		System.out.println(myTextArray);
+	}
+}
