66e84870368f3d3a9595c3a31e45f3fe726bb1c4
James Farrugia <jamesfarrugia@users.noreply.github.com>
2015-10-16 18:59:00 +0200

    Creating Picture sampler

---
+++ unit-2-variables-and-methods/HellowWorldPicture.java
@@ -0,0 +1,24 @@
+/**
+ * Hello world program.  Creates an image and calculates the area.
+ */
+public class HelloWorldPicture
+{
+	/**
+	 * Main method that does the work
+	 * 
+	 * @param args array with command line arguments
+	 */
+	public static void main(String[] args)
+	{
+		// Declaring the image
+		Picture myPicture = new Picture();
+		// Set the width and height of the myPicture instance
+		myPicture.width = 10;
+		myPicture.height = 10;
+		
+		int area = myPicture.width * myPicture.height;
+		myPicture.pixels = new Pixel[area];
+	  // Print out the area (width times height)
+		System.out.println("Area = " + area);
+	}
+}
